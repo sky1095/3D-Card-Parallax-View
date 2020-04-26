1a5bd5449bd784e986a69bc964462c561f78134e
Sudhanshu Kumar Yadav <sudhanshuexcel.yadav@gmail.com>
2020-04-26 18:59:10 +0530

    changes in main

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: '3D Card Parallax View',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
       home: Carousel(),
     );
